1c9db23abe31b3efe352e0047f6d9b3905419d33
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-12-07 10:48:04 -0800

    Solo Explore Done

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-// TODO
+const gList = ['Cucumbers', 'Kale', 'Almonds', 'Coffee Beans']
+
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false,
+    };
+  };
+
+  onItemClick (e) {
+    this.setState({
+      done: !this.state.done
+    })
+  };
+
+  render() {
+    const style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    }
+
+    return (
+      <li style={style} onClick={this.onItemClick.bind(this)}>{this.props.groceries}</li>
+    )
+  };
+}
+
+
+function App() {
+  const allItems = () => gList.map(items => <GroceryList key={items} groceries={items} />);
+  return (
+    <div>
+      <h2>My Todo List</h2>
+      <ul>
+        {allItems()}
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+// function GroceryList(props) {
+
+//   const onListClickHandler = (e) => {
+//     console.log('I got clicked');
+//   }
+
+//   const groceries = props.groceries.map((item) => <li key={item} onClick={onListClickHandler}>{item}</li>)
+
+//   return(
+//     <ul>
+//       {groceries}
+//     </ul>
+//   );
+// }
+
